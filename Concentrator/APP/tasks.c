dfafb2476248b483dd6701daa36c212114ebe642
YuQuan-Liu <liuyuquan08ouc@gmail.com>
2017-05-22 18:25:18 +0800

    同步到surfacee

--- Concentrator/APP/tasks.c
+++ Concentrator/APP/tasks.c
@@ -428,16 +428,12 @@ void Task_LORA(void *p_arg){
               //check the frame cs
               if(*(buf-2) == check_cs(buf_+6,frame_len-8)){
                 //the frame is ok;
-                switch(*(buf_+AFN_POSITION)){
-                case AFN_CONTROL:
-                case AFN_CURRENT:
-                  *buf = 0x02;//标识这一帧数据是来自LORA的
-                  OSQPost(&Q_Read,
-                          buf_,frame_len,
-                          OS_OPT_POST_FIFO,
-                          &err);
-                  break;
-                }
+                *buf = 0x02;//标识这一帧数据是来自LORA的
+                OSQPost(&Q_Deal,
+                        buf_,frame_len,
+                        OS_OPT_POST_FIFO,
+                        &err);
+                
                 if(err == OS_ERR_NONE){
                   buf_ = 0;
                   buf = 0;
@@ -544,147 +540,7 @@ void Task_LORA(void *p_arg){
 
 
 /**
- * 连接服务器 处理GPRS模块发送过来的指令
- */
-void Task_Server(void *p_arg){
-  OS_ERR err;
-  
-  uint8_t * buf_server_task = 0;
-  uint8_t * buf_server_task_ = 0;
-  uint8_t getbuffail = 0;
-  uint8_t connectfail = 0;
-  
-  while(DEF_TRUE){
-    if(connectstate == 1){
-      if(buf_server_task == 0){
-        buf_server_task = OSMemGet(&MEM_Buf,
-                       &err);
-        
-        if(err == OS_ERR_NONE){
-          //get the buf
-          getbuffail = 0;
-          buf_server_task_ = buf_server_task;
-          Mem_Set(buf_server_task_,0x00,256); //clear the buf
-          Server_Post2Buf(buf_server_task_);
-        }else{
-          //didn't get the buf
-          getbuffail ++;
-          if(getbuffail >= 20){
-            //20次没有获取到buf  
-            Device_Cmd(DISABLE);
-            *((uint8_t *)0) = 0x00;  //迫使系统重启
-          }
-          continue;
-        }
-      }
-      
-      if(server_ptr - server_ptr_ > 0){
-        OSTimeDly(4,
-             OS_OPT_TIME_DLY,
-             &err);
-        
-        buf_server_task = server_ptr;
-        check_str(buf_server_task_,buf_server_task);  //屏蔽掉数据前的0x00
-        if(Str_Str(buf_server_task_,"\n>")){
-          
-          buf_server_task = buf_server_task_;
-          Mem_Set(buf_server_task_,0x00,256); //clear the buf
-          Server_Post2Buf(buf_server_task_);
-          
-          OSSemPost(&SEM_Send,
-                    OS_OPT_POST_1,
-                    &err);
-          continue;
-        }
-        
-        if(Str_Str(buf_server_task_,"RECEIVE")){
-          //oh it's the data 
-          OSQPost(&Q_Deal,
-                  buf_server_task_,
-                  buf_server_task-buf_server_task_,
-                  OS_OPT_POST_FIFO,
-                  &err);
-          buf_server_task = 0;
-          Server_Post2Buf(0);
-          continue;
-        }
-        
-        //CLOSED
-        if(Str_Str(buf_server_task_,"CLOSE")){
-          
-          Mem_Set(buf_server_task_,0x00,256); //clear the buf
-          OSMemPut(&MEM_Buf,buf_server_task_,&err);
-          buf_server_task = 0;
-          buf_server_task_ = 0;
-          Server_Post2Buf(0);
-          change_connect(0);
-          continue;
-        }
-        //+PDP: DEACT\r\n
-        if(Str_Str(buf_server_task_,"DEACT")){
-          
-          Mem_Set(buf_server_task_,0x00,256); //clear the buf
-          OSMemPut(&MEM_Buf,buf_server_task_,&err);
-          buf_server_task = 0;
-          buf_server_task_ = 0;
-          Server_Post2Buf(0);
-          change_connect(0);
-          continue;
-        }
-        
-        if(Str_Str(buf_server_task_,"ERROR")){
-          
-          Mem_Set(buf_server_task_,0x00,256); //clear the buf
-          OSMemPut(&MEM_Buf,buf_server_task_,&err);
-          buf_server_task = 0;
-          buf_server_task_ = 0;
-          Server_Post2Buf(0);
-          change_connect(0);
-          continue;
-        }
-        
-        //don't know what's that
-        buf_server_task = buf_server_task_;
-        Mem_Set(buf_server_task_,0x00,256); //clear the buf
-        Server_Post2Buf(buf_server_task_);
-        
-      }else{
-        OSTimeDly(4,
-             OS_OPT_TIME_DLY,
-             &err);
-      }
-    }else{
-      
-      if(buf_server_task != 0){
-        Mem_Set(buf_server_task_,0x00,256); //clear the buf
-        OSMemPut(&MEM_Buf,buf_server_task_,&err);
-        
-        buf_server_task = 0;
-        buf_server_task_ = 0;
-        
-        Server_Post2Buf(0);
-      }
-      
-      Device_Cmd(DISABLE);
-      Device_Cmd(ENABLE);
-      if(connect() == SUCCESS){
-        connectfail = 0;
-      }else{
-        connectfail++;
-        if(connectfail > 20){
-          Device_Cmd(DISABLE);
-          *((uint8_t *)0) = 0x00;  //迫使系统重启
-        }
-      }
-    }
-  }
-  
-}
-
-
-
-/**
- * 处理服务器发送过来的指令
+ * 处理集中器发送过来的指令
  */
 void Task_DealServer(void *p_arg){
   CPU_TS ts;
@@ -699,7 +555,7 @@ void Task_DealServer(void *p_arg){
   
   while(DEF_TRUE){
     /*
-    \r\n+TCPRECV:0,**,0x68 L L 0x68 C A     \r\n
+    0x68 L L 0x68 C A     \r\n
     */
     buf_ptr_ = OSQPend(&Q_Deal,
             0,
@@ -708,156 +564,70 @@ void Task_DealServer(void *p_arg){
             &ts,
             &err);
     
-    start = Str_Str(buf_ptr_,"\r\n\x68") + 2;
+    start = buf_ptr_;
     
     //check the frame
     len = check_frame(start);
     
-      if(len){
-        //the frame is ok
-        switch(*(start+AFN_POSITION)){
-        case AFN_ACK:
-          //the ack of the server
-          
-          server_seq_ = *(start+SEQ_POSITION) & 0x0F;  //获得该帧的序列号
-          
-          if(server_seq_ == heart_seq){
-            OSSemPost(&SEM_HeartBeat,
+    if(len){
+      //the frame is ok
+      switch(*(start+AFN_POSITION)){
+      case AFN_ACK:
+        //the ack of the Concentrator
+        
+        server_seq_ = *(start+SEQ_POSITION) & 0x0F;  //获得该帧的序列号
+        
+        if(server_seq_ == data_seq){
+          OSSemPost(&SEM_ACKData,
                     OS_OPT_POST_1,
                     &err);
-          }else{
-            if(server_seq_ == data_seq){
-              OSSemPost(&SEM_ACKData,
-                        OS_OPT_POST_1,
-                        &err);
-            }else{
-              //抛弃此应答帧
-            }
-          }
-          break;
-        case AFN_CONFIG:
-        case AFN_QUERY:
-          buf_copy = OSMemGet(&MEM_Buf,&err);
-          if(buf_copy != 0){
-            Mem_Copy(buf_copy,start,len);
-            *(buf_copy + len) = 0x01;  //标识这一帧来自服务器
-            OSQPost(&Q_Config,buf_copy,len,OS_OPT_POST_1,&err);
-            if(err != OS_ERR_NONE){
-              OSMemPut(&MEM_Buf,buf_copy,&err);
-            }
-          }
-          break;
-        case AFN_CONTROL:
-        case AFN_CURRENT:
-          
-          server_seq_ = *(start + SEQ_POSITION) & 0x0F;
-          if(*(start+FN_POSITION) == 0x05){
-            //匹配序列号
-            server_seq = server_seq_;
-            device_ack(0x01,server_seq_);
-          }else{
-            if(server_seq != server_seq_){
-              //新的抄表指令  ack & read
-              buf_copy = OSMemGet(&MEM_Buf,&err);
-              if(buf_copy != 0){
-                Mem_Copy(buf_copy,start,len);
-                *(buf_copy + len) = 0x01;  //标识这一帧来自服务器
-                server_seq = server_seq_;
-                device_ack(0x01,server_seq_);
-                OSQPost(&Q_Read,buf_copy,len,OS_OPT_POST_1,&err);
-                if(err != OS_ERR_NONE){
-                  OSMemPut(&MEM_Buf,buf_copy,&err);
-                }
-              }
-            }else{
-              device_ack(0x01,server_seq_);
-            }
-          }
-          
-          
-          break;
-        case AFN_LINK_TEST:
-          //never will come to here
-          break;
-        case AFN_HISTORY:
-          //don't support
-          break;
-          
+        }else{
+          //抛弃此应答帧
         }
+        break;
+      case AFN_CONTROL:
+      case AFN_CURRENT:
+        
+        //首先判断是不是找自己的
+        
+        //如果是找自己 判断自己是否在抄表  
+        //如果不在抄表  去抄表
+        //如果在抄表   直接返回ACK
+        
+        
+        
+        server_seq_ = *(start + SEQ_POSITION) & 0x0F;
+        if(*(start+FN_POSITION) == 0x05){
+          //匹配序列号
+          server_seq = server_seq_;
+          device_ack(0x01,server_seq_);
+        }else{
+          if(server_seq != server_seq_){
+            //新的抄表指令  ack & read
+            buf_copy = OSMemGet(&MEM_Buf,&err);
+            if(buf_copy != 0){
+              Mem_Copy(buf_copy,start,len);
+              *(buf_copy + len) = 0x01;  //标识这一帧来自服务器
+              server_seq = server_seq_;
+              device_ack(0x01,server_seq_);
+              OSQPost(&Q_Read,buf_copy,len,OS_OPT_POST_1,&err);
+              if(err != OS_ERR_NONE){
+                OSMemPut(&MEM_Buf,buf_copy,&err);
+              }
+            }
+          }else{
+            device_ack(0x01,server_seq_);
+          }
+        }
+        
+        
+        break;
       }
+    }
     OSMemPut(&MEM_Buf,buf_ptr_,&err);
   }
 }
 
-
-/**
- * GPRS 心跳任务
- */
-void Task_HeartBeat(void *p_arg){
-  OS_ERR err;
-  CPU_TS ts;
-  uint8_t * buf_frame;
-  uint8_t heart_ack = 0;
-  uint8_t i;
-  uint8_t beat[17];
-  
-  while(DEF_TRUE){
-    buf_frame = beat;
-    *buf_frame++ = FRAME_HEAD;
-    //buf_frame_16 = (uint16_t *)buf_frame;
-    *buf_frame++ = 0x27;//(9 << 2) | 0x03;
-    *buf_frame++ = 0x00;
-    *buf_frame++ = 0x27;//(9 << 2) | 0x03;
-    *buf_frame++ = 0x00;
-    //buf_frame = (uint8_t *)buf_frame_16;
-    *buf_frame++ = FRAME_HEAD;
-    
-    *buf_frame++ = ZERO_BYTE | DIR_TO_SERVER | PRM_START | START_FUN_TEST;
-    /**/
-    *buf_frame++ = deviceaddr[0];
-    *buf_frame++ = deviceaddr[1];
-    *buf_frame++ = deviceaddr[2];
-    *buf_frame++ = deviceaddr[3];
-    *buf_frame++ = deviceaddr[4];
-    
-    *buf_frame++ = AFN_LINK_TEST;
-    
-    heart_seq = addSEQ();
-    *buf_frame++ = ZERO_BYTE |SINGLE | CONFIRM | heart_seq;
-    
-    *buf_frame++ = FN_HEARTBEAT;
-    
-    *buf_frame++ = check_cs(beat+6,9);
-    *buf_frame++ = FRAME_END;
-    if(connectstate){
-      for(i = 0;connectstate && i < 3;i++){
-        heart_ack = 0;
-        if(send_server(beat,17)){
-          OSSemPend(&SEM_HeartBeat,
-                    5000,
-                    OS_OPT_PEND_BLOCKING,
-                    &ts,
-                    &err);
-          if(err == OS_ERR_NONE){
-            heart_ack = 1;
-            break;
-          }
-        }
-      }
-      if(heart_ack){
-        OSTimeDly(120000,
-                    OS_OPT_TIME_DLY,
-                    &err);
-      }else{
-        change_connect(0);
-      }
-    }else{
-      OSTimeDly(100,
-                    OS_OPT_TIME_DLY,
-                    &err);
-    }
-  }
-}
 
 
 /**
@@ -931,10 +701,6 @@ void Task_LORA_Send(void *p_arg){
   while(DEF_TRUE){
     
     if(lora_send){
-      Write_LORA("TEST",4); //定时发送供采集器测试信号使用
-      OSTimeDly(3000,
-                OS_OPT_TIME_DLY,
-                &err);
       LED3_ON();
       OSTimeDly(500,
                 OS_OPT_TIME_DLY,
@@ -1101,38 +867,6 @@ void Task_LED(void *p_arg){
       OSTimeDly(100,
                 OS_OPT_TIME_DLY,
                 &err);
-      /*
-      if(readingall){
-        cnt++;
-        if(cnt >= 15){
-          cnt = 0;
-          buf_frame = readingbeat;
-          *buf_frame++ = FRAME_HEAD;
-          //buf_frame_16 = (uint16_t *)buf_frame;
-          *buf_frame++ = 0x27;//(9 << 2) | 0x03;
-          *buf_frame++ = 0x00;
-          *buf_frame++ = 0x27;//(9 << 2) | 0x03;
-          *buf_frame++ = 0x00;
-          //buf_frame = (uint8_t *)buf_frame_16;
-          *buf_frame++ = FRAME_HEAD;
-          
-          *buf_frame++ = ZERO_BYTE | DIR_TO_SERVER | PRM_START | START_FUN_REQ1;
-          
-          *buf_frame++ = deviceaddr[0];
-          *buf_frame++ = deviceaddr[1];
-          *buf_frame++ = deviceaddr[2];
-          *buf_frame++ = deviceaddr[3];
-          *buf_frame++ = deviceaddr[4];
-          
-          *buf_frame++ = AFN_FAKE;
-          *buf_frame++ = ZERO_BYTE |SINGLE | local_seq;
-          *buf_frame++ = readingall_progress;//FN_HEARTBEAT;
-          
-          *buf_frame++ = check_cs(readingbeat+6,9);
-          *buf_frame++ = FRAME_END;
-          send_server(readingbeat,17);
-        }
-      }*/
     }
   }
 }
