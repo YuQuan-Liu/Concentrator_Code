a97268f705b8db306cc4d92eb76c991a1bf916b8
YuQuan-Liu <liuyuquan08ouc@gmail.com>
2017-06-10 18:37:59 +0800

    集中器抄表之前清空config_flash

--- Concentrator/APP/tasks.c
+++ Concentrator/APP/tasks.c
@@ -972,7 +972,7 @@ void meter_read_eg(uint8_t * buf_frame,uint8_t frame_len,uint8_t desc){
     return;
   }
   meterdata = config_flash; //将表返回的所有信息存放在config_flash
-  
+  Mem_Set(config_flash,0x00,0x1000); //clear the buf
   
   if(cjq_ok){
     //cjq is ok wait the data
